9710e19203348135666cbb1b5df1c83315a261aa
ymelero <y.melero@creaf.uab.cat>
2024-03-01 17:13:41 +0100

    NA

--- PopStruct.R
+++ PopStruct.R
@@ -1,3 +1,10 @@
 ###### URBA POPULATION STRUCTURE ANALYSES CODE - MEDYCI ######
+library(usethis)
+library(gitcreds)
+create_github_token() # to create a token and link project and github
+gitcreds_set()
+use_github()
 
 library(tidyverse)
+
+
