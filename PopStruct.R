980597b079263e843adcbe1e947e747c77d32fc6
ymelero <y.melero@creaf.uab.cat>
2024-03-01 18:45:28 +0100

    NA

--- PopStruct.R
+++ PopStruct.R
@@ -1,4 +1,6 @@
 ###### URBA POPULATION STRUCTURE ANALYSES CODE - MEDYCI ######
+# project (created first also in GH), and linked via:
+# if doubts see: https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r/
 library(usethis)
 library(gitcreds)
 create_github_token() # to create a token and link project and github
